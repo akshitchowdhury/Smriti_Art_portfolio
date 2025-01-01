6e825bf0a9e07f8ce5f2907255795eb395bc4255
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2025-01-01 16:54:33 +0530

    About

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
-import HeroBg from "../../assets/heroBg.jpg"
+import "./Hero.css"
 import HeroText from './HeroText'
 const Hero = () => {
   return (
     <>
-    <div className='relative w-full h-[600px]' >
-  <img src={HeroBg} className='object-cover w-full h-full mix-blend-multiply' alt="Background Image"/>
-  <div className='absolute inset-0 bg-black opacity-65'></div>
+    <div class="hero-section">
+  <div class="hero-content">
+    <HeroText/>
+  </div>
 </div>
-<HeroText/>
+
 </>
   )
 }
