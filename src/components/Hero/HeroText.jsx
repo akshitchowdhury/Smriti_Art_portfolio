968614fb26bed0f7cab2a7292d6a2116794505ca
Pratikshit Chowdhury <akshitchowdhury@gmail.com>
2025-01-04 04:55:47 +0530

    GalleryCategories6

--- src/components/Hero/HeroText.jsx
+++ src/components/Hero/HeroText.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import "./HeroText.css"
 const HeroText = () => {
   return (
-    <div className="container absolute -my-[50%] lg:-my-[0%] md:-my-[0%] md:labsolute md:top-0
-     lg:absolute lg:right-72 lg:top-[200px]">
+    <div className="container absolute -my-[1%] lg:-my-[0%] md:-my-[0%] md:labsolute md:top-0
+     lg:absolute lg:right-72 lg:top-[200px] md:-left-[310px] left-[10px] top-[480px]">
       <div className="text-left px-4 md:mx-[30%] lg:w-[600px]">
     
     
         <h1 className="font-script text-4xl text-white font-semibold mb-2 md:text-5xl lg:text-[100px] ">
   <span className='text-[180px]'>S</span>mriti <span className="text-amber-400 ">Lekha </span>
-  <span className="text-blue-200 ">Gogoi</span>
+  <span className="text-blue-200 ">Gogoi's</span> <span className='text-2xl'>Art Page</span>
 </h1>
         
         {/* <button className="w-44 h-12 max-w-xs px-4 py-2  bg-amber-400  text-zinc-950 
